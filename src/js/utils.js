feaa402d6dcd3f303cf022bb7ed5ac6fe73b6bfc
kharioki <tony.kharioki@gmail.com>
2023-01-07 06:01:32 +0300

    filter and sort posts

--- src/js/utils.js
+++ src/js/utils.js
@@ -15,3 +15,45 @@ export function formatDate(date) {
     timeZone: 'UTC',
   });
 }
+
+export function formatBlogPosts(posts, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}) {
+
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    // filter out drafts
+    if (filterOutDrafts && draft) return acc;
+
+    // filter out future posts
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to accumulator
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  // sort posts by date
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => {
+      const dateA = new Date(a.frontmatter.date);
+      const dateB = new Date(b.frontmatter.date);
+
+      return dateB - dateA;
+    });
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit posts if number is provided
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
+}
