60abacef3942332a5dfb6d9b8b276667b3789022
kharioki <tony.kharioki@gmail.com>
2023-01-16 19:29:17 +0300

    rss config

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+
+import { formatBlogPosts } from '../js/utils.js';
+
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
+const posts = formatBlogPosts(Object.values(postImportResult));
+
+export const get = () => rss({
+  title: 'Kiki’s Blog',
+  description: 'The code is all out there, I just arrange it in a financially rewarding way.',
+  site: import.meta.env.SITE,
+  items: posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.date,
+    description: post.frontmatter.description,
+    customData: `
+      <author>${post.frontmatter.author}</author>
+    `
+  }))
+});
