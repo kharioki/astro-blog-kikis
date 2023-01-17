6991b64de6f172c0aedb3b7479b21478cf7aad30
kharioki <tony.kharioki@gmail.com>
2023-01-17 08:04:22 +0300

    styling rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,8 @@ const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
 const posts = formatBlogPosts(Object.values(postImportResult));
 
 export const get = () => rss({
+  // use stylesheet from "public/rss/styles.xsl"
+  stylesheet: '/rss/styles.xsl',
   title: 'Kiki’s Blog',
   description: 'The code is all out there, I just arrange it in a financially rewarding way.',
   site: import.meta.env.SITE,
